8413891dabe6931409cac6dcf15d03ba338b071f
jbxamora <jbxamora@icloud.com>
2023-04-22 01:03:55 -0700

    created box that displays summarized content

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -43,7 +43,7 @@ const handleSubmit = async (e) => {
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
-      <div className="fkex flex-col w-full gap-2">
+      <div className="flex flex-col w-full gap-2 ">
         <form
           className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
@@ -90,6 +90,29 @@ const handleSubmit = async (e) => {
 
       </div>
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+        <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">Well, that wasn't supposed to happen...
+          <br />
+          <span className="font-satoshi font-normal text-gray-700">
+            {error?.data?.error}
+          </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 }
