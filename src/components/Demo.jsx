9fe10666d8c6be890d185ca828c8c26f28fbe563
jbxamora <jbxamora@icloud.com>
2023-04-22 00:17:31 -0700

    updated api // local storage logic

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,14 +2,42 @@ import { useState, useEffect } from "react"
 
 import { copy, linkIcon, loader, tick } from "../assets"
 
+import { useLazyGetSummaryQuery } from "../services/article"
+
 const Demo = () => {
   const [article, setArticle] = useState({
     url: '',
     summary: '',
-  })
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+useEffect(() => {
+  const articlesFromLocalStorage = JSON.parse(
+    localStorage.getItem('articles')
+  )
+
+  if (articlesFromLocalStorage) {
+    setAllArticles(articlesFromLocalStorage)
+  }
+}, []);
 
 const handleSubmit = async (e) => {
-  alert('Submitted');
+  e.preventDefault();
+
+  const { data } = await getSummary({ articleUrl: article.url });
+
+  if(data?.summary) {
+    const newArticle = { ...article, summary: data.summary }
+    const updatedAllArticles = [newArticle, ...allArticles];
+
+    setArticle(newArticle);
+    setAllArticles(updatedAllArticles);
+
+    localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+  }
 }
 
   return (
