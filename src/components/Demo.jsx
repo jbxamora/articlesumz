51f635365c6097239d7cf8b6a3a56a943e304ea4
jbxamora <jbxamora@icloud.com>
2023-04-21 23:30:48 -0700

    set up store using redux rdtk

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,13 +3,22 @@ import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick } from "../assets"
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+const handleSubmit = async (e) => {
+  alert('Submitted');
+}
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="fkex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -19,8 +28,9 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter An Article URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...
+            article, url: e.target.value })}
             required
             className="url_input peer"
           />
@@ -31,7 +41,7 @@ const Demo = () => {
           </button>
         </form>
         {/* Browser URL History */}
-        
+
       </div>
       {/* Display Results */}
     </section>
