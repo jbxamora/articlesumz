48d48c5b41ba46da7bed08d9539a2a2965e60a55
jbxamora <jbxamora@icloud.com>
2023-04-22 01:12:46 -0700

    updated demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -101,7 +101,7 @@ const handleCopy = (copyUrl) => {
         {isFetching ? (
         <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
         ) : error ? (
-          <p className="font-inter font-bold text-black text-center">Well, that wasn't supposed to happen...
+          <p className="font-inter font-bold text-black text-center">Well, that was not supposed to happen...
           <br />
           <span className="font-satoshi font-normal text-gray-700">
             {error?.data?.error}
